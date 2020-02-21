0c90d61fcdc56c0fd1c4f5c09f07b0a5bf719b46
Nicholas Roberto <njr444@gmail.com>
2020-02-21 12:11:01 -0800

    fixed images

--- data/beers.js
+++ data/beers.js
@@ -3,7 +3,7 @@ module.exports = [
         name: 'Galactic Wrath',
         brewery: 'Dust Bowl brewing',
         style: 'IPA',
-        url:'public/ipa.png',
+        url:'https://brewerydb-images.s3.amazonaws.com/brewery/GN1WbX/upload_PVIN3S-squareLarge.png',
         abv: 7.1,
         is_season: true
     },
@@ -11,7 +11,7 @@ module.exports = [
         name: 'Apathy',
         brewery: 'Green Bush',
         style: 'Stout',
-        url:'public/stout.png',
+        url:'https://brewerydb-images.s3.amazonaws.com/beer/TGlWU4/upload_DkxW63-large.png',
         abv: 8.4,
         is_season: true
     },
@@ -19,7 +19,7 @@ module.exports = [
         name: 'Big Bad Wolf',
         brewery: 'Grimm Brothers',
         style: 'Imperial Red Ale',
-        url:'public/IRA.png',
+        url:'https://brewerydb-images.s3.amazonaws.com/beer/2msVKZ/upload_DilIvz-large.png',
         abv: 6.5,
         is_season: true
     },
@@ -27,7 +27,7 @@ module.exports = [
         name: 'hazy lager',
         brewery: 'home republic',
         style: 'Lager',
-        url:'public/lager.png',
+        url:'https://brewerydb-images.s3.amazonaws.com/brewery/QzesuU/upload_kLoeHg-squareLarge.png',
         abv: 4.2,
         is_season: false
     },
@@ -35,7 +35,7 @@ module.exports = [
         name: 'Bj porter',
         brewery: '3 brothers',
         style: 'Porter',
-        url: 'public/porter.png',
+        url: 'https://brewerydb-images.s3.amazonaws.com/brewery/LWD44j/upload_dWVveA-squareLarge.png',
         abv: 9.2,
         is_season: true
     },
@@ -43,7 +43,7 @@ module.exports = [
         name: 'Arizona trail ale',
         brewery: 'That brewery',
         style: 'Pale Ale',
-        url:'public/pale ale.png',
+        url:'https://brewerydb-images.s3.amazonaws.com/beer/BHezTP/upload_gKTzsW-large.png',
         abv: 5.5,
         is_season: false
     },
@@ -51,7 +51,7 @@ module.exports = [
         name: 'Autobahn',
         brewery: 'Crabtree Brewing',
         style: 'Dunkel',
-        url: 'public/dunkel.png',
+        url: 'https://brewerydb-images.s3.amazonaws.com/brewery/l2V4B0/upload_VVNTKu-squareLarge.png',
         abv: 8,
         is_season: true
     }
