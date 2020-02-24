282ed398568de0696af9bf3c0a8df007e449dafa
Nicholas Roberto <njr444@gmail.com>
2020-02-24 15:25:18 -0800

    add put and delete route

--- app.js
+++ app.js
@@ -54,6 +54,31 @@ app.post('/api/beers', async(req, res) => {
         });
     }
 });
+
+app.put('/api/beers', async(req, res) => {
+    // using req.body instead of req.params or req.query (which belong to /GET requests)
+    try {
+        // make a new cat out of the cat that comes in req.body;
+        const result = await client.query(`
+            UPDATE beer 
+            SET 
+                name = ${req.body.name},
+                brewery = ${req.body.brewery},
+                style_id = ${req.body.style},
+                url = ${req.body.url},
+                abv = ${req.body.abv},
+                is_season = ${req.body.is_season}
+            WHERE id = ${req.body.id};
+        `,
+        );
+        res.json(result.rows[0]); // return just the first result of our query
+    }
+    catch (err) {
+        res.status(500).json({
+            error: err.message || err
+        });
+    }
+});
 app.get('/api/beer/:beerId', async(req, res) => {
     try {
         const result = await client.query(`
@@ -80,6 +105,23 @@ app.get('/api/styles', async(req, res) => {
             ORDER BY name;
         `);
 
+        res.json(result.rows);
+    }
+    catch (err) {
+        res.status(500).json({
+            error: err.message || err
+        });
+    }
+});
+
+app.delete('/api/beer/:beerId', async(req, res) => {
+    try {
+        const result = await client.query(`
+            DELETE 
+            FROM beer
+            WHERE id=$1`, //req.params.beerID ** 
+         
+        [req.params.beerId]);
         res.json(result.rows);
     }
     catch (err) {
